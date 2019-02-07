a232cd598cdc8d7117305efc54e7cd08efd4d424
RyuSA <ryusa@RyuSA.local>
2019-02-07 23:25:19 +0900

    add dashboard sample

---
+++ components/dashboard/jobCard.vue
@@ -0,0 +1,73 @@
+<template>
+    <v-expansion-panel>
+        <v-expansion-panel-content v-for="job in jobs" :key="job.name">
+            <div slot="header">{{ job.name }}</div>
+            <v-card class="pa-4">
+                <v-layout>
+                    <v-chip outline color="accent">{{ job.schedule.type }}</v-chip>
+                    <v-chip outline color="primary" v-if="job.is_active == true">runnning</v-chip>
+                    <v-chip outline color="error" v-else>stop</v-chip>
+                </v-layout>
+                <v-list dense>
+                    <v-list-tile v-if="job.schedule.type == 'weekly'">
+                        <v-list-tile-content>曜日:</v-list-tile-content>
+                        <v-list-tile-content class="align-end">{{ job.schedule.day }}</v-list-tile-content>
+                    </v-list-tile>
+                    <v-list-tile>
+                        <v-list-tile-content>実行時刻:</v-list-tile-content>
+                        <v-list-tile-content class="align-end">{{ job.exec_time }}</v-list-tile-content>
+                    </v-list-tile>
+                    <v-list-tile>
+                        <v-list-tile-content>作成日:</v-list-tile-content>
+                        <v-list-tile-content class="align-end">{{ job.created_at }}</v-list-tile-content>
+                    </v-list-tile>
+                    <v-list-tile>
+                        <v-list-tile-content>更新日時:</v-list-tile-content>
+                        <v-list-tile-content class="align-end">{{ job.updated_at }}</v-list-tile-content>
+                    </v-list-tile>
+                </v-list>
+            </v-card>
+        </v-expansion-panel-content>
+    </v-expansion-panel>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            jobs: [
+                {
+                    name: "job1",
+                    schedule: {
+                        type: "weekly",
+                        day: "Mon"
+                    },
+                    exec_time: "10:00",
+                    updated_at: "2019-01-01 11:00:00",
+                    created_at: "2019-01-01 10:00:00",
+                    is_active: true
+                },
+                {
+                    name: "job2",
+                    schedule: {
+                        type: "dayly"
+                    },
+                    exec_time: "10:00",
+                    updated_at: "2018-01-01 12:34:56",
+                    created_at: "2018-01-01 01:23:45",
+                    is_active: true
+                },
+                {
+                    name: "job3",
+                    schedule: {
+                        type: "once"
+                    },
+                    exec_time: "10:00",
+                    updated_at: "2019-01-01 11:00:00",
+                    created_at: "2019-01-01 10:00:00",
+                    is_active: false
+                }
+            ]
+        };
+    }
+};
+</script>
---
+++ pages/dashboard/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-app>
+        <v-container>
+            <job-card/>
+        </v-container>
+    </v-app>
+</template>
+
+<script>
+import jobCard from "~/components/dashboard/jobCard.vue";
+
+export default {
+    components: {
+        jobCard
+    }
+};
+</script>
+
+<style scoped>
+.test {
+    margin: 40px auto;
+    width: 90%;
+    padding: 20px;
+}
+</style>
